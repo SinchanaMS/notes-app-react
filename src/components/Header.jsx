09884a6c107659414ec4e95678ad0a9f0b693d6e
SinchanaMS <94738532+SinchanaMS@users.noreply.github.com>
2022-07-07 22:25:22 +0530

    fix: clear filters

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,49 +1,89 @@
-import { Link, NavLink } from 'react-router-dom'
-import { useAuth, useNote, useFilter } from '../contexts/contexts'
-import mainLogo from "../assets/images/mainLogo.png"
+import { Link, NavLink } from "react-router-dom";
+import { useAuth, useNote, useFilter } from "../contexts/contexts";
+import mainLogo from "../assets/images/mainLogo.png";
 
 export default function Header() {
-  const {loggedIn, logoutHandler} = useAuth()
-  const {allLabelsList} = useNote()
-  const {filterState, filterDispatch} = useFilter()
+  const { loggedIn, logoutHandler } = useAuth();
+  const { allLabelsList } = useNote();
+  const { filterState, filterDispatch } = useFilter();
 
-  function handleLogin(e){
-    loggedIn  ?  logoutHandler() : navigate("/")
+  function handleLogin(e) {
+    loggedIn ? logoutHandler() : navigate("/");
   }
 
   return (
-  <div className='header shadow'>
-    <div className='brand-logo'>
-      <img src={mainLogo} />
-      <NavLink to="/" className="p-lg"> Jotter </NavLink>
-    </div>
-    <nav className="nav-links">
-      {loggedIn ? 
-      <Link to="/" onClick={handleLogin}> Logout </Link> :
-      <div className='auth-opts'>
-        <Link to="/login"> Login </Link> 
-        <Link to="/signup"> Sign Up </Link> 
-      </div>}
-    </nav>
-
-    {loggedIn &&  
-    <div className="sidebar shadow">
-      <nav className="side-nav">
-        <NavLink to="/"> Home </NavLink>
-        <NavLink to="/archive"> Archives </NavLink>
-        <NavLink to="/trash"> Trash </NavLink>
-        <NavLink to="/" > Labels
-          {allLabelsList.map(label => (
-            label.length!==0 &&
-            <div className="labels" style={{backgroundColor: filterState.labels.includes(label) && "var(--HOVER-BG)"}}>
-              <span class="material-icons material-icons-outlined label">label</span>
-              <p type="checkbox" data-value = {label} onClick={(e) => filterDispatch({type: "LABEL", payload: e.target.dataset.value})}> {label} </p>
-            </div>
-          ))}
+    <div className="header shadow">
+      <div className="brand-logo">
+        <img src={mainLogo} />
+        <NavLink to="/" className="p-lg">
+          {" "}
+          Jotter{" "}
         </NavLink>
+      </div>
+      <nav className="nav-links">
+        {loggedIn ? (
+          <Link to="/" onClick={handleLogin}>
+            {" "}
+            Logout{" "}
+          </Link>
+        ) : (
+          <div className="auth-opts">
+            <Link to="/login"> Login </Link>
+            <Link to="/signup"> Sign Up </Link>
+          </div>
+        )}
       </nav>
+
+      {loggedIn && (
+        <div className="sidebar shadow">
+          <nav className="side-nav">
+            <NavLink to="/"> Home </NavLink>
+            <NavLink to="/archive"> Archives </NavLink>
+            <NavLink to="/trash"> Trash </NavLink>
+            <NavLink to="/">
+              <div className="filters">
+                <p>Labels</p>
+                <p
+                  className="clear-filter"
+                  onClick={(e) => filterDispatch({ type: "CLEAR" })}
+                >
+                  Clear Filters
+                </p>
+              </div>
+              {allLabelsList.map(
+                (label) =>
+                  label.length !== 0 && (
+                    <div
+                      className="labels"
+                      style={{
+                        backgroundColor:
+                          filterState.labels.includes(label) &&
+                          "var(--HOVER-BG)",
+                        padding: "5px 10px",
+                      }}
+                    >
+                      <span class="material-icons material-icons-outlined label">
+                        label
+                      </span>
+                      <p
+                        type="checkbox"
+                        data-value={label}
+                        onClick={(e) =>
+                          filterDispatch({
+                            type: "LABEL",
+                            payload: e.target.dataset.value,
+                          })
+                        }
+                      >
+                        {label}
+                      </p>
+                    </div>
+                  )
+              )}
+            </NavLink>
+          </nav>
+        </div>
+      )}
     </div>
-    }
-  </div>
-  )
+  );
 }
--- src/components/SortBy.jsx
+++ src/components/SortBy.jsx
@@ -1,31 +1,77 @@
-import "../styles/sortby.css"
-import {useState} from "react"
-import { useFilter } from "../contexts/FilterContext"
-import { useAuth } from "../contexts/AuthContext"
+import "../styles/sortby.css";
+import { useState } from "react";
+import { useFilter } from "../contexts/FilterContext";
+import { useAuth } from "../contexts/AuthContext";
 
 export default function SortBy() {
-    
-    const [showSort, setShowSort] = useState(false)
-    const {filterDispatch} = useFilter()
-    const {loggedIn} = useAuth()
-    
-    return (
-        <div className='sort-by'>
-             {loggedIn &&
-            <>
-            <p className="sort-title" onClick={() =>setShowSort(!showSort)}>Sort By:</p>
-            <div className={showSort ? 'sort-options shadow active' : 'sort-options shadow'}>
-                <div className="sort-option">Date Created
-                    <p className='p-sm sort' data-value="NEWEST-TO-OLDEST" onClick={() => filterDispatch({type: "SORTBY", payload: "NEWEST-TO-OLDEST"})}>Newest to Oldest</p>
-                    <p className='p-sm sort' data-value="OLDEST-TO-NEWEST" onClick={() => filterDispatch({type: "SORTBY", payload: "OLDEST-TO-NEWEST"})}>Oldest to Newest</p>
-                </div>
+  const [showSort, setShowSort] = useState(false);
+  const { filterDispatch } = useFilter();
+  const { loggedIn } = useAuth();
 
-                <div className="sort-option">Priority
-                    <p className='p-sm sort' data-value="High to Low" onClick={() => filterDispatch({type: "SORTBY", payload: "HIGH-TO-LOW"})}>High to Low</p>
-                    <p className='p-sm sort' data-value="Low to High" onClick={() => filterDispatch({type: "SORTBY", payload: "LOW-TO-HIGH"})}>Low to High</p>
-                </div>
+  return (
+    <div className="sort-by">
+      {loggedIn && (
+        <>
+          <p className="sort-title" onClick={() => setShowSort(!showSort)}>
+            Sort By:
+          </p>
+          <div
+            className={
+              showSort ? "sort-options shadow active" : "sort-options shadow"
+            }
+          >
+            <div className="sort-option">
+              Date Created
+              <p
+                className=" sort"
+                data-value="NEWEST-TO-OLDEST"
+                onClick={() =>
+                  filterDispatch({
+                    type: "SORTBY",
+                    payload: "NEWEST-TO-OLDEST",
+                  })
+                }
+              >
+                Newest to Oldest
+              </p>
+              <p
+                className=" sort"
+                data-value="OLDEST-TO-NEWEST"
+                onClick={() =>
+                  filterDispatch({
+                    type: "SORTBY",
+                    payload: "OLDEST-TO-NEWEST",
+                  })
+                }
+              >
+                Oldest to Newest
+              </p>
             </div>
-            </>}
-        </div>
-    )
+
+            <div className="sort-option">
+              Priority
+              <p
+                className=" sort"
+                data-value="High to Low"
+                onClick={() =>
+                  filterDispatch({ type: "SORTBY", payload: "HIGH-TO-LOW" })
+                }
+              >
+                High to Low
+              </p>
+              <p
+                className=" sort"
+                data-value="Low to High"
+                onClick={() =>
+                  filterDispatch({ type: "SORTBY", payload: "LOW-TO-HIGH" })
+                }
+              >
+                Low to High
+              </p>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
